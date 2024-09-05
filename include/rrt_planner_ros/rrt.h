2ac35ba5c06f0fbc8b8091aa179cbfcdc9e4d0df
mulempyo <luckyp1@naver.com>
2024-09-05 23:27:40 +0900

    failed planning

--- include/rrt_planner_ros/rrt.h
+++ include/rrt_planner_ros/rrt.h
@@ -1,25 +1,21 @@
 #ifndef RRT_PLANNER_
 #define RRT_PLANNER_
 
+#include <base_local_planner/costmap_model.h>
+#include <base_local_planner/world_model.h>
 #include <nav_core/base_global_planner.h>
 #include <costmap_2d/costmap_2d.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf2/LinearMath/Quaternion.h>
 
-namespace my_global_planner {
+namespace rrt {
 
 class RRTPlanner : public nav_core::BaseGlobalPlanner {
-
-struct Node {
-        double x, y;
-        Node* parent;
-        Node(double _x, double _y, Node* _parent = nullptr) : x(_x), y(_y), parent(_parent) {}
-    };
-
 public:
     RRTPlanner();
     RRTPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+    ~RRTPlanner();
 
     void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros) override;
     bool makePlan(const geometry_msgs::PoseStamped& start, 
@@ -27,22 +23,24 @@ public:
                   std::vector<geometry_msgs::PoseStamped>& plan) override;
 
 private:
-    std::vector<Node*> tree;
-    bool isCollision(double x, double y);
-    Node* getRandomNode();
-    Node* nearestNode(Node* random_node);
-    std::vector<Node*> getNeighbors(Node* node);
-    double distance(double x1, double y1, double x2, double y2);
-    void mapToWorld(double mx, double my, double& wx, double& wy);
-    void worldToMap(double wx, double wy, double& mx, double& my);
 
+    double footprintCost(double x, double y, double th) const;
+    bool isValidPose(double x, double y, double th) const;
+    void createRandomValidPose(double &x, double &y, double &th) const;
+    unsigned int nearestNode(double random_x, double random_y);
+    double distance(double x1, double y1, double x2, double y2);
+    void mapToWorld(unsigned int mx, unsigned int my, double& wx, double& wy);
+
+    std::vector<unsigned int> tree;
     costmap_2d::Costmap2D* costmap_;
+    costmap_2d::Costmap2DROS* costmap_ros_;
     double origin_x_, origin_y_, resolution_;
     unsigned int width_, height_;
     bool initialized_;
     double goal_threshold_;
     double step_size_;
     unsigned int max_iterations_;
+    base_local_planner::CostmapModel* world_model_;
  };
 };
 #endif
