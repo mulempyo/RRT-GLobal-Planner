26db2915bcb401f2eeddf90f5de7a5453572d9a3
mulempyo <luckyp1@naver.com>
2024-09-09 00:07:31 +0900

    success rrt planning

--- include/rrt_planner_ros/rrt.h
+++ include/rrt_planner_ros/rrt.h
@@ -1,6 +1,7 @@
 #ifndef RRT_PLANNER_
 #define RRT_PLANNER_
 
+#include <ros/ros.h>
 #include <base_local_planner/costmap_model.h>
 #include <base_local_planner/world_model.h>
 #include <nav_core/base_global_planner.h>
@@ -17,6 +18,8 @@ public:
     RRTPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
     ~RRTPlanner();
 
+    ros::Publisher plan_pub_;
+
     void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros) override;
     bool makePlan(const geometry_msgs::PoseStamped& start, 
                   const geometry_msgs::PoseStamped& goal, 
@@ -26,12 +29,19 @@ private:
 
     double footprintCost(double x, double y, double th) const;
     bool isValidPose(double x, double y, double th) const;
-    bool createRandomValidPose(double &x, double &y, double &th) const;
-    int nearestNode(double random_x, double random_y);
+    void createRandomValidPose(double &x, double &y, double &th) const;
+    unsigned int nearestNode(double random_x, double random_y);
+    void createPoseWithinRange(double start_x, double start_y, double start_th,
+                                       double end_x, double end_y, double end_th,
+                                       double range,
+                                       double &new_x, double &new_y, double &new_th) const;
+    bool isValidPathBetweenPoses(double x1, double y1, double th1,
+                                         double x2, double y2, double th2) const;
+    void publishPlan(const std::vector<geometry_msgs::PoseStamped> &path) const;
     double distance(double x1, double y1, double x2, double y2);
     void mapToWorld(unsigned int mx, unsigned int my, double& wx, double& wy);
 
-    std::vector<unsigned int> tree;
+    std::vector<std::pair<unsigned int, unsigned int>> tree;
     costmap_2d::Costmap2D* costmap_;
     costmap_2d::Costmap2DROS* costmap_ros_;
     double origin_x_, origin_y_, resolution_;
